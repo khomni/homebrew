eb32ea567501caf2d05d90a58b44f917cad5004f
Kyle <kylejhalvorson@gmail.com>
2016-03-30 14:43:04 -0700

    fix up vignettes

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/create', function(req,res,next){
-  res.
+router.post('/create', function(req,res,next){
+  console.log(req.body);
+  var user = models.User.create({
+    username: req.body.username,
+    email: req.body.email,
+  })
+  .then(function(user){
+    console.log('[model.User]', user)
+    res.render('/'+user.id,{user: user})
+  })
+  .catch(function(err) {
+    next(err);// Ooops, do some error-handling
+  })
 })
 
 router.post('/login', function(req,res,next){
-  
+  passport.authenticate('local',function(err,user,info){
+
+  })
 })
 
 module.exports = router;
