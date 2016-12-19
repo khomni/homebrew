66734be1b2a94a2996412ac6e74a48d3bd0546a6
Kyle <kylejhalvorson@gmail.com>
2016-12-18 23:08:29 -0800

    nearby items

--- routes/character/items.js
+++ routes/character/items.js
@@ -19,8 +19,7 @@ router.get('/', Common.middleware.requireCharacter, (req, res, next) => {
 
   return Promise.props({
     owned: res.locals.character.getItems({order: [order]}),
-    near: db.Item.findAll({
-      attributes: {exclude: ['CharacterId']},
+    near: db.Item.count({
       where: [{CharacterId:null},db.sequelize.fn('ST_DWithin', db.sequelize.col('location'), db.sequelize.fn('ST_MakePoint', here[0], here[1]),10000),]
     })
   })
@@ -35,11 +34,28 @@ router.get('/', Common.middleware.requireCharacter, (req, res, next) => {
       return a
     },{value:0, weight:0, total:0})
 
-    if(req.requestType('json')) return res.json(results)
+    if(req.requestType('json')) return res.json({items: results.owned, total: meta})
     return res.render('characters/inventory/index',{meta:meta})
   })
   .catch(next)
 });
+
+router.get('/nearby', Common.middleware.requireCharacter, (req,res,next) => {
+
+  var here = req.user.activeChar.location.coordinates
+
+  return db.Item.findAll({
+    attributes: {exclude: ['CharacterId']},
+    where: [{CharacterId:null},db.sequelize.fn('ST_DWithin', db.sequelize.col('location'), db.sequelize.fn('ST_MakePoint', here[0], here[1]),10000),]
+  }).then(items => {
+    if(req.requestType('json')) return res.json(items)
+    if(req.requestType('modal')) return res.render('characters/inventory/modals/give',{items:items})
+
+  })
+  .catch(next)
+
+
+})
 
 router.get('/new', Common.middleware.requireCharacter, (req, res, next) => {
 
@@ -72,7 +88,10 @@ router.post('/give', Common.middleware.requireCharacter, (req,res,next) => {
     return Promise.map(items, item => {
       return req.user.activeChar.hasItems(item)
       .then(owned => {
-        if(owned) return res.locals.character.addItem(item)
+        if(owned || !item.CharacterId) {
+          item.location = null
+          return res.locals.character.addItem(item)
+        }
         return null;
       })
     })
