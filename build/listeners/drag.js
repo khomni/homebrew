acc7196338287a7d5fc30899da7fa2b4961e3a43
Kyle <kylejhalvorson@gmail.com>
2017-03-07 09:34:45 -0800

    upload drop areas

--- build/listeners/drag.js
+++ build/listeners/drag.js
@@ -4,20 +4,24 @@ var dom = require('./dom.js');
 
 (() => {
 
+  function mouseOut(e){
+    console.log('mouseout')
+    document.body.classList.remove('dropfile')
+  }
+
   window.addEventListener('dragenter', e => {
-    if (e.target.tagName != "INPUT") {
-      document.body.classList.remove('dropfile');
-      return e.preventDefault();
-    }
-    console.log(e)
+    window.addEventListener('mouseout', mouseOut)
     document.body.classList.add('dropfile');
-  },false)
+  }, false)
 
   window.addEventListener('dragend', e => {
+    console.log('exit:', e.target)
+    window.removeEventListener('mouseout', mouseOut)
     document.body.classList.remove('dropfile');
   },false)
 
   window.addEventListener('drop', e => {
+    window.removeEventListener('mouseout', mouseOut)
     if (e.target.tagName != "INPUT") return e.preventDefault();
     console.log('drop:',e.dataTransfer.files);
     document.body.classList.remove('dropfile');
