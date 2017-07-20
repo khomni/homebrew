5121a23ea97fff8f702bc1d505aaccebcfc5db30
Kyle <kylejhalvorson@gmail.com>
2017-07-19 22:46:41 -0700

    Webpack build process

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'public/javascripts');
+const APP_DIR = path.resolve(__dirname, 'build/react');
+const LOCAL = process.env.NODE_ENV === 'local';
+
+let config = {
+  entry: {
+    bundle: ['babel-polyfill', APP_DIR + '/index.jsx'],
+  },
+  output: {
+    path: BUILD_DIR,
+    publicPath: '/',
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader'
+      },
+      // {
+      //   test: /\.(css)/,
+      //   include: APP_DIR,
+      //   use: LOCAL
+      //     ? [ 'style-loader', 'css-loader' ]
+      //     : ExtractTextPlugin.extract({
+      //       fallback: "style-loader",
+      //       use: "css-loader"
+      //     })
+      // },
+      // {
+      //   test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      //   loader: 'url-loader',
+      //   options: {
+      //     limit: 10000
+      //   }
+      // },
+      // {
+      //   test: /\.(md|txt)$/,
+      //   use: ['raw-loader']
+      // }
+    ]
+  },
+  plugins: [],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
+
+module.exports = config;
+
